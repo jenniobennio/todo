6377e32c0f1e99b497b78e905f1b8fef5891d3b1
Jenny Kwan <jennylkwan@gmail.com>
2014-01-25 17:36:14 -0800

    Initial Commit

---
+++ todo/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  todo
+//
+//  Created by Jenny Kwan on 1/25/14.
+//  Copyright (c) 2014 Jenny Kwan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ todo/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  todo
+//
+//  Created by Jenny Kwan on 1/25/14.
+//  Copyright (c) 2014 Jenny Kwan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
